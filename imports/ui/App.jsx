2297648c2f5c137396a42f1ad713ee2306f55f5f
Todd Hancock <thancock20@hotmail.com>
2016-08-02 17:27:38 -0500

    Finish hide complete functionality

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
       hideCompleted: false
     };
   }
-  
+
   handleSubmit(event) {
     event.preventDefault();
 
@@ -31,8 +31,18 @@ class App extends Component {
     ReactDOM.findDOMNode(this.refs.textInput).value = '';
   }
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted
+    });
+  }
+
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
