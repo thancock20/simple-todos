33c8ae04083c0149d29f98e8c548c1fcea1d1932
Todd Hancock <thancock20@hotmail.com>
2016-08-03 12:35:24 -0500

    Update renderTasks to pass in showPrivateButton

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -42,7 +42,16 @@ class App extends Component {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
     return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
     ));
   }
 
@@ -91,7 +100,7 @@ App.propTypes = {
 
 export default createContainer(() => {
   Meteor.subscribe('tasks');
-  
+
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 }}).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
