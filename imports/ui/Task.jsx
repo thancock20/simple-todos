034547360a3c1097af752186286cf72fbc4ee956
Todd Hancock <thancock20@hotmail.com>
2016-08-03 12:46:12 -0500

    Fix typos in Task.jsx and App.jsx

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -12,6 +12,10 @@ export default class Task extends Component {
     Meteor.call('tasks.remove', this.props.task._id);
   }
 
+  togglePrivate() {
+    Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+  }
+
   render() {
     // Give tasks a different className when they are checked off,
     // so that we can style them nicely in CSS
@@ -30,6 +34,12 @@ export default class Task extends Component {
           onClick={this.toggleChecked.bind(this)}
         />
 
+      { this.props.showPrivateButton ? (
+        <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+          { this.props.task.private ? 'Private' : 'Public' }
+        </button>
+      ) : '' }
+
         <span className="text">
           <strong>{this.props.task.username}</strong>: {this.props.task.text}</span>
       </li>
@@ -40,5 +50,6 @@ export default class Task extends Component {
 Task.propTypes = {
   // This component gets the task to display through a React prop.
   // We can use propTypes to indicate it is required
-  task: PropTypes.object.isRequired
+  task: PropTypes.object.isRequired,
+  showPrivateButton: React.Proptypes.bool.isRequired,
 };
